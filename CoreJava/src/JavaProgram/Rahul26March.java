db9861e978a489cf9d453f80e8dc62d05d992c11
RAHUL <RAHUL@RAHUL-PC>
2022-03-26 21:19:42 +0530

    my first repo commit

---
+++ CoreJava/src/JavaProgram/Rahul26March.java
@@ -0,0 +1,10 @@
+package JavaProgram;
+
+public class Rahul26March {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiiiiiiiiiiii");
+		System.out.println("byyyyyyyyyyyyyyyyyyy");
+	}
+
+}
