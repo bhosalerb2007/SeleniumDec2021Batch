3e4d1e1a83963ba0bf7b04dc8d890c5003190c05
LENOVO <LENOVO@DESKTOP-N3OHL39>
2022-07-01 19:45:58 +0530

    Simple Java Program

---
+++ src/SeleniumCode/SimpleProg.java
@@ -0,0 +1,10 @@
+package SeleniumCode;
+
+public class SimpleProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is simple program of sysout function");
+	}
+
+}
